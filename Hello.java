4219d276e0046afb84cc30b4d0469202c876f02c
Iqbal Hossain Mostakim <hossain.iqbal3@northsouth.edu>
2021-03-01 02:42:04 +0600

    Added a new method called display to hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main(String[] args) {
+    System.out.println("Vs code is awesome...");
+    greeting("Iqbal Hossain")
+    
+}
+public static void greeting(String name)
+{
+    System.out.println("Welcome " + name);
+}
+
+}
